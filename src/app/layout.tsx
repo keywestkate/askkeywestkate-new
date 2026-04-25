6b9d20210bcccbe54e18eb47cb8b912cc6871b98
Kate Baldwin <kate@keywestkate.com>
2026-04-25 16:54:36 -0400

    Revert to Modern Serif font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Fraunces } from "next/font/google";
 import "./globals.css";
-
-const fraunces = Fraunces({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "900"],
-  style: ["normal", "italic"],
-  variable: "--font-fraunces",
-  display: "swap",
-});
 
 const modernSerif = localFont({
   src: [
@@ -47,7 +38,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${fraunces.variable} ${modernSerif.variable} h-full antialiased`}
+      className={`${modernSerif.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
